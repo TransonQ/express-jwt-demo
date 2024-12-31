42e7311928f31d1df3c092d8b26862e398eb8ef7
TransonQ <quanscheng@qq.com>
2024-12-31 16:12:07 +0800

    更新 Home 组件

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,5 +2,9 @@ import { useFetchHello } from '../hooks/useFetchHello';
 
 export function Home() {
   useFetchHello();
-  return <div className="text-orange-400">Home</div>;
+  return (
+    <div className="bg-orange-400 text-white rounded-2xl p-4 mt-4 mx-4">
+      Home
+    </div>
+  );
 }
